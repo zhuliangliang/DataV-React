69e54e93e322acfdfbc12ba5985d93cdb8b9f655
zhuliangliang <zhuliangliang@mucang.cn>
2021-06-02 11:40:11 +0800

    测试

--- src/components/scrollBoard/index.js
+++ src/components/scrollBoard/index.js
@@ -152,6 +152,7 @@ function calcAligns(mergedConfig, header) {
 }
 
 const ScrollBoard = forwardRef(({ onClick, config = {}, className, style, onMouseOver }, ref) => {
+  console.log("zhuliangliang")
   const { width, height, domRef } = useAutoResize(ref)
 
   const [state, setState] = useState({
